8b46bdcba80b4e1926f795829826078f4db64d4f
laffra <laffra@gmail.com>
2017-12-05 22:19:05 +0100

    Choose local file names based on uid

--- storage.py
+++ storage.py
@@ -32,7 +32,7 @@ from collections import defaultdict
 HOME_DIR = os.path.join(os.path.expanduser('~'), 'IKKE')
 HOME_DIR_SEGMENT_COUNT = len(HOME_DIR.split(os.path.pathsep))
 CLEANUP_FILE_NAME_RE = '([^a-zA-Z0-9]|http|www|https)'
-SHORT_DIR = os.path.join(HOME_DIR, 'items')
+ITEMS_DIR = os.path.join(HOME_DIR, 'items')
 
 GET_COMMENT_SCRIPT = '''osascript<<END
     tell application "Finder"
@@ -80,7 +80,6 @@ class Storage:
     def add_data(cls, data):
         # type: (dict) -> None
         cls.add_file(
-            cls.get_filename(data),
             json.dumps(data),
             data,
             "w"
@@ -90,40 +89,38 @@ class Storage:
     def add_binary_data(cls, content, data):
         # type: (bytes,dict) -> None
         cls.add_file(
-            cls.get_filename(data, extension=''),
             content,
             data,
             "wb"
         )
 
     @classmethod
-    def get_local_path(cls, path):
-        # type: (str) -> str
-        local_path = os.path.join(HOME_DIR, path)
-        if os.name == 'nt':
-            if os.path.sep in path:
-                local_path = os.path.join(SHORT_DIR, '%s.txt' % uuid.uuid4())
-            local_path = '\\\\?\\' + local_path
+    def get_local_path(cls, obj):
+        # type: (dict) -> str
+        local_path = os.path.join(os.path.join(ITEMS_DIR, obj['kind']), obj['uid'])
+        if obj['kind'] != 'file':
+            local_path += '.txt'
+        parent_dir = os.path.dirname(local_path)
         try:
-            os.makedirs(os.path.dirname(local_path))
+            os.makedirs(parent_dir)
         except:
-            pass # ignore if dirs already exist
+            pass # allow multiple threads to create the dir at the same time
         return local_path
 
     @classmethod
-    def add_file(cls, filename, body, data, format="wb"):
-        # type: (str,bytes,dict,str) -> None
+    def add_file(cls, body, data, format="wb"):
+        # type: (bytes,dict,str) -> None
         assert 'kind' in data, "Kind needed"
         assert 'uid' in data, "UID needed"
         assert 'timestamp' in data, "Timestamp needed"
         assert type(data['timestamp']) in (int,float), "Number timestamp needed, not %s" % type(data['timestamp'])
         cls.stats['writes'] += 1
-        path = cls.get_local_path(filename)
+        path = cls.get_local_path(data)
+        # print('STORAGE: write %s - %s %s' % (path, data['kind'], data['uid']))
         try:
             with open(path, format) as fout:
                 fout.write(body)
             os.utime(path, (data['timestamp'], data['timestamp']))
-            obj = cls.resolve(path)
         except IOError as e:
             print(e)
             raise
@@ -137,7 +134,7 @@ class Storage:
             return ['cscript', '/nologo', os.path.join(local_dir, 'search.vbs'), HOME_DIR, query, since]
         elif os.name == 'posix':
             operator = '-interpret'
-            since = 'modified:%s-%s' % (cls.get_day_month_year(days), cls.get_day_month_year(0))
+            since = 'modified:%s-%s' % (cls.get_day_month_year(days), cls.get_day_month_year(-1))
             return ['mdfind', '-onlyin', HOME_DIR, operator, query, since]
         else:
             raise ValueError('Unsupported OS:', os.name)
@@ -151,39 +148,26 @@ class Storage:
         paths = list(cls.run_command(cls.get_search_command(query, days)))
         resolve_start = time.time()
         results = list(filter(None, map(cls.resolve, paths)))
-        cls.log_search_stats(len(paths), time.time() - search_start, len(results), time.time() - resolve_start)
+        cls.log_search_stats(query, len(paths), time.time() - search_start, len(results), time.time() - resolve_start)
         return results
 
     @classmethod
-    def log_search_stats(cls, search_count, search_duration, resolve_count, resolve_duration):
+    def log_search_stats(cls, query,  search_count, search_duration, resolve_count, resolve_duration):
         cls.stats['searches'] += 1
         cls.stats['raw results'] = search_count
         cls.stats['search time'] += search_duration
         cls.stats['results'] = resolve_count
         cls.stats['resolve time'] += resolve_duration
+        cls.print_search_stats(query)
+
+    @classmethod
+    def print_search_stats(cls, query=None):
         print('STORAGE: %s STATS %s' % ('#'*32, '#'*32))
+        if query:
+            print('STORAGE: query: "%s"' % query)
         for k,v in cls.stats.items():
             print('STORAGE: %s: %s' % (k, v))
         print('STORAGE: %s' % ('#'*71))
-
-    @classmethod
-    def get_filename(cls, data, extension='.txt'):
-        # type: (dict,str) -> str
-        handler = import_module('importers.%s' % data['kind'])
-        def serialize(value):
-            if isinstance(value, list):
-                quoted = [quote(v.encode('utf8'), safe='') for v in value]
-                return '[%s%s%s%s' % (os.path.sep, len(quoted), os.path.sep, os.path.sep.join(quoted))
-            if isinstance(value, float):
-                return '%f' % value
-            if isinstance(value, int):
-                return '%d' % value
-            return quote(('%s' % value).encode('utf8'), '')[:250]
-        kv = [(k,serialize(v)) for k,v in data.items() if k in handler.PATH_ATTRIBUTES and v]
-        kv = sorted(kv, key=lambda kv: kv[0])
-        segments = os.path.sep.join(itertools.chain(*kv)).split(os.path.sep)
-        path = os.path.sep.join(segments)
-        return os.path.join(data['kind'], '%s%s' % (path, extension))
 
     @classmethod
     def search_contact(cls, email):
@@ -208,66 +192,26 @@ class Storage:
         # type: (str) -> list
         return cls.search(filename, operator='-name')
 
-
     @classmethod
     def resolve(cls, path):
         # type: (str) -> (dict,None)
+        if not path or not os.path.isfile(path):
+            # print('STORAGE: skip non file: %s' % path[len(HOME_DIR):])
+            return None
         item = cls.file_cache.get(path)
         if not item:
-            if path.startswith(SHORT_DIR):
-                with open(path) as f:
-                    text = '???'
-                    obj = None
-                    try:
-                        text = f.read()
-                        Storage.stats['items read'] += 1
-                        obj = json.loads(text)
-                    except UnicodeDecodeError as e:
-                        Storage.stats['failed'] += 1
-                        print('STORAGE: json error %s: %s' % (e, path))
-                    except JSONDecodeError as e:
-                        Storage.stats['failed'] += 1
-                        print('STORAGE: json error %s: %s' % (e, path))
-            else:
-                kv = cls.split_path(path)
-                kind, kv = kv[0], kv[1:]
-                if kind == 'file':
-                    obj = File(path)
-                else:
-                    keys = []
-                    values = []
-                    index = 0
-                    while index < len(kv)-1:
-                        key = kv[index]
-                        value = kv[index+1]
-                        if value == '[':
-                            count = int(kv[index+2])
-                            value = kv[index+3:index+3+count]
-                            index += count + 1
-                        keys.append(key)
-                        values.append(value)
-                        index += 2
-                    if values:
-                        values[-1] = values[-1][:-4]
-                    obj = dict(zip(keys, [v for v in values]))
-                    if not 'uid' in obj:
-                        return None
-                    obj['kind'] = kind
-                    obj['label'] = obj.get('label', obj['uid'])
-                    obj['words'] = obj['label'].split(' ')
-                    t = obj.get('timestamp', get_timestamp())
-                    obj['timestamp'] = float(obj.get('timestamp', get_timestamp()))
+            with open(path) as f:
+                obj = None
+                try:
+                    obj = json.loads(f.read())
+                    Storage.stats['items read'] += 1
+                except Exception as e:
+                    Storage.stats['files'] += 1
+                    item = File(path)
             if obj:
                 item = cls.to_item(obj, path)
-                cls.file_cache[path] = item
+            cls.file_cache[path] = item
         return item
-
-    @classmethod
-    def split_path(cls, path):
-        return [
-            unquote(p).replace('+',' ')
-            for p in path[len(HOME_DIR) + 1:].split(os.path.sep)
-        ]
 
     @classmethod
     def to_item(cls, obj, path):
@@ -343,6 +287,9 @@ class Data(dict):
     def update(self, obj):
         obj['timestamp'] = self.timestamp
 
+    def update_words(self, items):
+        pass
+
     def is_duplicate(self, duplicates):
         return False
 
@@ -373,6 +320,15 @@ class File(Data):
             self.zoomed_icon_size = 256
         self.icon = 'get?path=%s' % icon
         dict.update(self, vars(self))
+        self.set_message_id(path)
+
+    def set_message_id(self, path):
+        self.message_id = os.path.basename(os.path.dirname(path))
+
+    def update_words(self, items):
+        for item in items:
+            if item.kind == 'gmail' and item.message_id == self.message_id:
+                self.words = item.words
 
 
 def delete_all(kind):
@@ -393,12 +349,15 @@ if __name__ == '__main__':
                 print('%s=%s' % (k,v))
         print()
 
-    if True:
+    if False:
         results = sorted((os.path.getmtime(result.path),result.path) for result in Storage.search('laffra', days=5))
         now = datetime.datetime.now()
         for timestamp,path in results[:5]:
             print('%s  %s' % (now - datetime.datetime.fromtimestamp(timestamp), path))
         print()
+
+    if True:
+        File('/Users/laffra/IKKE/items/file/<CALA7AgVq6zqJarWf9gY+r+kPQMxqZnp3JSQovhuAh6=DryGjTg@mail.gmail.com>/Trip on 16 Nov 17 - PNR ref AMMF2I.pdf')
 
     if False:
         for k,v in Storage.search_contact('laffra@gmail.com').items():
